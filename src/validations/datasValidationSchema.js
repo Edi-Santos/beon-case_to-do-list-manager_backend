604c0a8e726df53df412723b5e34ec755d1b7756
Ed Santos <edsantos2822@gmail.com>
2022-06-22 15:38:38 -0300

    build: adicionando informação para validação de dados

--- src/validations/datasValidationSchema.js
+++ src/validations/datasValidationSchema.js
@@ -5,5 +5,5 @@ module.exports = Joi.object({
   deadline: Joi.string().required(),
   status: Joi.string().required(),
   priority: Joi.string().required(),
-  finished: Joi.string(),
+  finished: Joi.string().min(0),
 });
